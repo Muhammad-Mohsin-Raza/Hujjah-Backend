9d4f6761d5d52402d0ec961002ce981be9309de7
Muhammad-Mohsin-Raza <mohsinraza1a@gmail.com>
2025-06-29 22:04:36 +0500

    commit'

--- hujjah_backend/hujjah_backend/settings.py
+++ hujjah_backend/hujjah_backend/settings.py
@@ -133,7 +133,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # React dev server
     "http://127.0.0.1:5173",
     "http://16.16.70.4:8001",
-    "https://sing-up-hojjah.vercel.app/"
+    "https://sing-up-hojjah.vercel.app",
+    "https://jasless-platform-9hk6.vercel.app"
 ]
 
 # Internationalization
